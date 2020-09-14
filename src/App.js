56da45b6086d93c03c04e49eb01e2c1a7fb577f3
Vlad Denisov <vlad.a.denisov@gmail.com>
2020-09-14 18:31:21 +0300

    fix: image request

--- src/App.js
+++ src/App.js
@@ -19,76 +19,76 @@ import '@vkontakte/vkui/dist/vkui.css';
 
 class App extends React.Component {
 
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			user: null
-		};
-	}
+        this.state = {
+            user: null
+        };
+    }
 
-	componentDidMount() {
-		bridge.send('VKWebAppGetUserInfo', {})
-			.then((user) => {
-				this.setState({ user });
-			});
-	}
+    componentDidMount() {
+        bridge.send('VKWebAppGetUserInfo', {})
+            .then((user) => {
+                this.setState({ user });
+            });
+    }
 
-	getImage = async () => {
+    getImage = async () => {
         const image = document.getElementById('url').value;
 
-        console.log({image});
+        console.log({ image });
 
         if (image) {
-			this.setState({ image });
-		}
-	};
+            this.setState({ image });
+        }
+    };
 
-	render() {
-		const { user, image } = this.state;
+    render() {
+        const { user, image } = this.state;
 
-		return (
+        return (
             <View activePanel="main">
                 <Panel id="main">
                     <PanelHeader>
-						{
+                        {
                             user ?
                                 <PanelHeaderContent
                                     status="VK Apps Image Example"
-                                    before={ <Avatar size={ 36 } src={ user.photo_100 } /> }
+                                    before={<Avatar size={36} src={user.photo_100} />}
                                 >
-                                    { user.first_name }
+                                    {user.first_name}
                                 </PanelHeaderContent>
-								:
-								'VK Apps Image Example'
-						}
+                                :
+                                'VK Apps Image Example'
+                        }
                     </PanelHeader>
 
-                    <Group header={ <Header mode="secondary">Задание</Header> }>
-						<Div>
-							У нас есть мини-приложение, которое имеет возможность загружать изображения по внешней ссылке. Увы, в приложении допущена ошибка, из-за чего удаленный сервер получает параметры запуска пользователей, которые вставляют ссылку. Нужно объяснить почему так происходит и исправить ситуацию красиво и лаконично.
+                    <Group header={<Header mode="secondary">Задание</Header>}>
+                        <Div>
+                            У нас есть мини-приложение, которое имеет возможность загружать изображения по внешней ссылке. Увы, в приложении допущена ошибка, из-за чего удаленный сервер получает параметры запуска пользователей, которые вставляют ссылку. Нужно объяснить почему так происходит и исправить ситуацию красиво и лаконично.
 						</Div>
-						<Div>
+                        <Div>
                             Для облегчения тестирования можно использовать картинку <Text weight="semibold">https://service.pavel.im/image</Text> (good code — все круто, bad code — сервер получил параметры запуска)
 						</Div>
 
                         <FormLayout>
                             <Input type="text" id="url" />
 
-                            <Button size="xl" onClick={ this.getImage }>Загрузить</Button>
+                            <Button size="xl" onClick={this.getImage}>Загрузить</Button>
                         </FormLayout>
 
-						{
-							!!image &&
-							<Div style={{ textAlign: 'center' }}>
-                                <img src={ image } alt="remote file" />
-							</Div>
-						}
+                        {
+                            !!image &&
+                            <Div style={{ textAlign: 'center' }}>
+                                <img src={image} alt="remote file" />
+                            </Div>
+                        }
                     </Group>
                 </Panel>
             </View>
-		);
-	}
+        );
+    }
 
 }
 
